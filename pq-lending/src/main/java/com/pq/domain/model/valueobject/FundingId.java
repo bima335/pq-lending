c3752a409334633d8e1cf31e94192bf3af244abf
fathdzul <mfatihizzulhaq@gmail.com>
2026-05-12 15:37:33 +0700

    Menambahkan semua value object yang belum dibuat
    semua id dibuat value object agar strongly typed id dan tidak tertukar tukar

---
+++ pq-lending/src/main/java/com/pq/domain/model/valueobject/FundingId.java
@@ -0,0 +1,13 @@
+package com.pq.domain.model.valueobject;
+
+public class FundingId {
+    private final String value;
+
+    public FundingId(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
---
+++ pq-lending/src/main/java/com/pq/domain/model/valueobject/LenderId.java
@@ -0,0 +1,13 @@
+package com.pq.domain.model.valueobject;
+
+public class LenderId {
+    private final String value;
+
+    public LenderId(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
